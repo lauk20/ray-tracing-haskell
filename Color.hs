b8bd17cd36c5e72a677143c117dde6656f4e86e8
lauk20 <72945813+lauk20@users.noreply.github.com>
2024-12-28 14:51:16 -0500

    add diffuse reflections and gamma correction of colors

--- Color.hs
+++ Color.hs
@@ -5,12 +5,18 @@ import Vec3
 
 type Color = Vec3
 
+-- Convert color to gamma scale
+linearToGamma :: Double -> Double
+linearToGamma linearComponent
+    | linearComponent > 0 = sqrt linearComponent
+    | otherwise = 0
+
 -- print out the RGB values
 writeColor :: Color -> IO ()
 writeColor (MkVec3 r g b) = do
     print $ PlainString (show rbyte ++ " " ++ show gbyte ++ " " ++ show bbyte)
     where 
         intensity = Interval 0.0 0.999
-        rbyte = floor $ 255.999 * clamp intensity r
-        gbyte = floor $ 255.999 * clamp intensity g
-        bbyte = floor $ 255.999 * clamp intensity b
+        rbyte = floor $ 255.999 * clamp intensity (linearToGamma r)
+        gbyte = floor $ 255.999 * clamp intensity (linearToGamma g)
+        bbyte = floor $ 255.999 * clamp intensity (linearToGamma b)
--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 module Main where
 import Color
+import Data.List (foldl')
 import Hittable
 import PlainString
 import Ray
@@ -24,6 +25,10 @@ samplesPerPixel :: Int
 samplesPerPixel = 100
 pixelSamplesScale :: Double
 pixelSamplesScale = 1.0 / fromIntegral samplesPerPixel
+
+-- Max Depth
+maxDepth :: Int
+maxDepth = 10
 
 -- Camera
 focalLength :: Double
@@ -72,24 +77,28 @@ gen = mkStdGen 42
 
 -- World of objects
 world = HittableList [
-        HittableObj (Sphere (MkVec3 0 0 (-1)) 0.5),
-        HittableObj (Sphere (MkVec3 0 (-100.5) (-1)) 100)
-    ]
+            HittableObj (Sphere (MkVec3 0 0 (-1)) 0.5),
+            HittableObj (Sphere (MkVec3 0 (-100.5) (-1)) 100)
+        ]
 
 -- Get Color of ray sent into scene
-rayColor :: Ray -> HittableList -> Color
-rayColor ray world =
-    case hitSomething of
-        Just hitRecord -> normal hitRecord .+ MkVec3 1 1 1 .* 0.5
-        Nothing -> (MkVec3 1.0 1.0 1.0 .* (1.0 - a)) .+ (MkVec3 0.5 0.7 1.0 .* a)
+rayColor :: (RandomGen g) => Ray -> HittableList -> g -> Int -> (Color, g)
+rayColor ray world gen iters =
+    if iters == 0
+    then (MkVec3 0 0 0, gen)
+    else
+        case hitSomething of
+            Just hitRecord -> let (direction, g1) = randomOnHemisphere (normal hitRecord) gen
+                            in (fst (rayColor (Ray (p hitRecord) (direction .+ (normal hitRecord))) world g1 (iters - 1)) .* 0.5, g1)
+            Nothing -> ((MkVec3 1.0 1.0 1.0 .* (1.0 - a)) .+ (MkVec3 0.5 0.7 1.0 .* a), gen)
     where 
         unitDirection = unitVector $ direction ray
         a = (y unitDirection + 1.0) * 0.5
-        hitSomething = hit world ray 0 (1 / 0) Nothing
+        hitSomething = hit world ray 0.001 (1 / 0) Nothing
 
 -- Construct a camera ray originating from the origin and directed at randomly sampled
 -- point around the pixel location i, j.
-getRay :: Int -> Int -> StdGen -> (Ray, StdGen)
+getRay :: (RandomGen g) => Int -> Int -> g -> (Ray, g)
 getRay i j gen =
     let 
         (offset, gen1) = sampleSquare gen
@@ -101,27 +110,40 @@ getRay i j gen =
     in (Ray rayOrigin rayDirection, gen1)
 
 -- Returns the vector to a random point in the [-.5,-.5]-[+.5,+.5] unit square.
-sampleSquare :: StdGen -> (Vec3, StdGen)
+sampleSquare :: (RandomGen g) => g -> (Vec3, g)
 sampleSquare gen =
     let (randX, gen1) = randomDouble gen
         (randY, gen2) = randomDouble gen1
     in (MkVec3 (randX - 0.5) (randY - 0.5) 0, gen2)
 
 -- Generate the RGB raster
-generateRGB :: [[Vec3]]
-generateRGB = 
-    [ [ (getMultipleSamples i j (MkVec3 0 0 0) gen) .* pixelSamplesScale
-        | i <- [0..imageWidth - 1] ]
-        | j <- [0..imageHeight - 1] ]
+-- Proper propagation of the random state
+generateRGB :: (RandomGen g) => g -> [[Vec3]]
+generateRGB gen =
+    let (pixels, _) = foldl' generateRow ([], gen) [0..imageHeight - 1]
+    in reverse pixels
     where
-        getMultipleSamples :: Int -> Int -> Color -> StdGen -> Color
-        getMultipleSamples i j pixelColor gen = helper i j pixelColor samplesPerPixel gen
-            where 
-                helper :: Int -> Int -> Color -> Int -> StdGen -> Color
-                helper i j pixelColor 0 gen = pixelColor
-                helper i j pixelColor iterations gen = helper i j (pixelColor .+ rayColor r world) (iterations - 1) gen'
-                    where (r, gen') = getRay i j gen
-            
+        generateRow :: (RandomGen g) => ([[Vec3]], g) -> Int -> ([[Vec3]], g)
+        generateRow (acc, g) j =
+            let (row, g') = foldl' (generatePixel j) ([], g) [0..imageWidth - 1]
+            in (reverse row : acc, g')
+        
+        generatePixel :: (RandomGen g) => Int -> ([Vec3], g) -> Int -> ([Vec3], g)
+        generatePixel j (acc, g) i =
+            let (color, g') = getMultipleSamples i j g
+            in (color .* pixelSamplesScale : acc, g')
+        
+        getMultipleSamples :: (RandomGen g) => Int -> Int -> g -> (Color, g)
+        getMultipleSamples i j g =
+            foldl' accumSamples (MkVec3 0 0 0, g) [1..samplesPerPixel]
+            where
+                accumSamples :: (RandomGen g) => (Color, g) -> Int -> (Color, g)
+                accumSamples (accColor, gen) _ =
+                    let 
+                        (ray, gen1) = getRay i j gen
+                        (sampleColor, gen2) = rayColor ray world gen1 maxDepth
+                    in (accColor .+ sampleColor, gen2)
+ 
 -- Main
 main :: IO ()
 main = do
@@ -130,4 +152,4 @@ main = do
     print $ PlainString ("P3\n" ++ show imageWidth ++ " " ++ show imageHeight ++ "\n255")
 
     -- Print RGB Raster
-    mapM_ (mapM_ writeColor) generateRGB
+    mapM_ (mapM_ writeColor) $ generateRGB gen
